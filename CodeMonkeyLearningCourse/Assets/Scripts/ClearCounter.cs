60459a3445e2ac74448c765b27629703b1d86984
Ciberdead <nico_goyon@orange.fr>
2024-04-16 15:10:55 +0100

    Scriptable objects done

--- CodeMonkeyLearningCourse/Assets/Scripts/ClearCounter.cs
+++ CodeMonkeyLearningCourse/Assets/Scripts/ClearCounter.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
     public void Interact() {
-        Debug.Log("Clearing counter");
+        Transform kitchenObjectSOTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectSOTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectSOTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName + " has been placed on the counter");
     }   
 }
